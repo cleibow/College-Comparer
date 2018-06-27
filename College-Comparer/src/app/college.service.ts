1c8c3bc32b477ca27f4fb91e378491a1e4bcced0
Cooper Leibow <cleibow@tulane.edu>
2018-06-27 10:33:46 -0500

    Created college component and college service

---
+++ College-Comparer/src/app/college.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CollegeService {
+
+  constructor(private _httpClient: HttpClient) { }
+
+
+
+}
